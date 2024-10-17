43bb6062b773855dd71a8762664cf67fb32a61b1
Bedasa <48552699+bedasawayessa@users.noreply.github.com>
2024-10-17 18:02:04 +0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth scroll functionality for navigation links
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        const targetSection = document.querySelector(this.getAttribute('href'));
+        window.scrollTo({
+            top: targetSection.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
